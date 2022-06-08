56a0f3e266c96371a76808139a313db7dfe9c795
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-06-08 15:11:25 +0300

    исправления

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -101,7 +101,8 @@ class ImageGallery extends Component {
       console.log(this.state);
 
       imagesApi
-        .fetchImages(this.props.searchValue, this.state.page)
+        // .fetchImages(this.props.searchValue, this.state.page)
+        .fetchImages(this.props.searchValue, 1)
         .then(results => {
           const { hits, total } = results;
 
@@ -119,8 +120,8 @@ class ImageGallery extends Component {
         .catch(error => this.setState({ error, status: Status.REJECTED }));
     }
 
-    // if (prevState.page !== this.state.page || this.state.page !== 1) {
-    if (prevState.page !== this.state.page) {
+    if (prevState.page !== this.state.page || this.state.page !== 1) {
+      // if (prevState.page !== this.state.page) {
       this.setState({ status: Status.PENDING });
 
       imagesApi
